6bafdb3db2c0cada29670cb11c98b971868c3dbd
bauer90 <us.huerhan@gmail.com>
2014-03-20 00:00:12 +0800

    setting up eclipse for java on win8.1

--- mar19/src/mar19/mar19.java
+++ mar19/src/mar19/mar19.java
@@ -1,9 +1,8 @@
 package mar19;
 
 public class mar19 {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-	}
-	
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        
+    }
 }
