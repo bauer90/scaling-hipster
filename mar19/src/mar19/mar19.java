fee47e11816bb6ca041735e297dd4bbe0c992f4c
bauer90 <us.huerhan@gmail.com>
2014-03-20 01:39:49 +0800

    finished 3.1 (fixed size stacks)

--- mar19/src/mar19/mar19.java
+++ mar19/src/mar19/mar19.java
@@ -3,6 +3,14 @@ package mar19;
 public class mar19 {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        
+        TripleStackUsingArray stacks = new TripleStackUsingArray(4);
+
+        stacks.push_to(2,2);
+        System.out.println(stacks.toString());
+        stacks.pop_from(2);
+        stacks.push_to(1, 44);
+        System.out.println(stacks.toString());
+        stacks.push_to(0, 3);
+        System.out.println(stacks.toString());
     }
 }
