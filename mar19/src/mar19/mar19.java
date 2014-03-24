4d70cc7274a46392d4ef525f960a6cbd1e73a755
bauer90 <us.huerhan@gmail.com>
2014-03-24 19:47:22 +0800

    push mar19.java in starbucks

--- mar19/src/mar19/mar19.java
+++ mar19/src/mar19/mar19.java
@@ -3,9 +3,9 @@ package mar19;
 public class mar19 {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        ArrayOfStacks tmp = new ArrayOfStacks(30);
-        for (int i = 0; i < 100; i++) {
-            tmp.push(3);
+        ArrayOfStacks tmp = new ArrayOfStacks(6);
+        for (int i = 0; i < 20; i++) {
+            tmp.push(i * 79 % 17);
         }
         System.out.println(tmp.toString());
     }
