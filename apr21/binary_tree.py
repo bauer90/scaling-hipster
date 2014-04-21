126fcff480d8fbac684b914fc155d34ff3043179
bauer90 <us.huerhan@gmail.com>
2014-04-22 00:41:55 +0800

    completed insert() for bst

--- apr21/binary_tree.py
+++ apr21/binary_tree.py
@@ -1,27 +1,48 @@
 class BinaryNode:
-	left, right, data = None, None, 0
+    left, right, data, parent = None, None, 0, None
 
-	def __init__(self, _data):
-		self.left = None
-		self.right = None
-		self.data = _data
+    def __init__(self, _data):
+        self.left = None
+        self.right = None
+        self.data = _data
+        self.parent = None
 
 class BinaryTree:
-	root = None
+    root = None
+    size = 0
 
-	def __init__(self):
-		self.root = None
+    def __init__(self):
+        self.root = None
+        self.size = 0
 
-	def is_empty(self):
-		return self.root == None
+    def is_empty(self):
+        return self.size == 0
 
-	def insert(self, _node, _data):
-		if self.is_empty():
-			self.root = BinaryNode(self.root, _data);
-		else:
-			if _data <= root.data:
-				self.insert(_node.left, _data)
-			else:
-				self.insert(_node.right, _data)
-		return
+    def insert(self, _data):
+        if self.is_empty():
+            self.root = BinaryNode(_data)
+            self.root.parent = self.root
+            self.size += 1
+        else:
+            cur = self.root
+            while 1:
+                if _data < cur.data:
+                    if cur.left == None:
+                        cur.left = BinaryNode(_data)
+                        cur.left.parent = cur
+                        self.size += 1
+                        break
+                    else:
+                        cur = cur.left
+                        continue
+                else:
+                    if cur.right == None:
+                        cur.right = BinaryNode(_data)
+                        cur.right.parent = cur
+                        self.size += 1
+                        break
+                    else:
+                        cur = cur.right
+                        continue
+        return
 
